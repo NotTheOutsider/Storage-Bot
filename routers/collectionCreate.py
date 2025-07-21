1ca5a8f213d2981af041efc0b7dfcefa62be2cd8
NotTheOutsider <egormalyshkin1@gmail.com>
2025-07-21 19:47:43 +0300

    Simple message clean up mechanism

--- routers/collectionCreate.py
+++ routers/collectionCreate.py
@@ -27,11 +27,14 @@ async def cmd_create_collection(message: Message, state: FSMContext):
             reply_markup=inline_keyboard
         )
     
+    await state.update_data(message_ids_to_delete=[sent_message.message_id, message.message_id])
     await state.set_state(Form.waiting_for_folder_name)
     
 @router.callback_query(F.data == "go_back")
 async def retun_to_main(callback: CallbackQuery, state: FSMContext):
-    await callback.message.delete()
+    data = await state.get_data()
+    message_ids_to_delete = data.get("message_ids_to_delete", [])
+    await callback.bot.delete_messages(chat_id=callback.message.chat.id, message_ids=message_ids_to_delete)
 
     await callback.message.answer(
         'As you say man, going back\\. Am just working here 🤠',
@@ -42,19 +45,32 @@ async def retun_to_main(callback: CallbackQuery, state: FSMContext):
 
 @router.message(F.text, Form.waiting_for_folder_name)
 async def collection_name_chosen(message: Message, state: FSMContext):
+    data = await state.get_data()
+    message_ids_to_delete = data.get("message_ids_to_delete", [])
+    message_ids_to_delete.append(message.message_id)
+
     collection_name = message.text.strip()
+
     if not collection_name:
-        sent_message = await message.answer(
+        error_message = await message.answer(
             'Do not act stupid\\. Are you for real trying to put *blank spaces* as the name of the collection? 🤨',
             parse_mode=ParseMode.MARKDOWN_V2
         )
+
+        message_ids_to_delete.append(error_message.message_id)
+        await state.update_data(message_ids=message_ids_to_delete)
         return
     if len(collection_name) > 25:
-        sent_message = await message.answer(
+        error_message = await message.answer(
             'Boooring 😴💤 Can you make it *shorter* man?',
             parse_mode=ParseMode.MARKDOWN_V2
         )
+
+        message_ids_to_delete.append(error_message.message_id)
+        await state.update_data(message_ids=message_ids_to_delete)
         return
+    
+    await message.bot.delete_messages(chat_id=message.chat.id, message_ids=message_ids_to_delete)
 
     await message.answer(
         f'New collection `{message.text}` has been created',
